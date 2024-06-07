22a5243df59e0afc0472206c092869d306566927
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-06-07 16:04:32 +0700

    add response formatter

---
+++ app/Helpers/ResponseFormatter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Format response.
+ */
+class ResponseFormatter
+{
+    /**
+     * API Response
+     *
+     * @var array
+     */
+    protected static array $response = [
+        "meta" => [
+            "code" => 200,
+            "status" => "success",
+            "message" => null
+        ],
+        "data" => null
+    ];
+
+    /**
+     * Give success response
+     *
+     * @param $data
+     * @param $message
+     * @return JsonResponse
+     */
+    public static function success($data = null, $message = null): JsonResponse
+    {
+        self::$response["meta"]["message"] = $message;
+        self::$response["data"] = $data;
+
+        return response()->json(self::$response, self::$response["meta"]["code"]);
+    }
+
+    /**
+     * Give error response
+     *
+     * @param $data
+     * @param $message
+     * @param int $code
+     * @return JsonResponse
+     */
+    public static function error($data = null, $message = null, int $code = 400): JsonResponse
+    {
+        self::$response["meta"]["status"] = "error";
+        self::$response["meta"]["code"] = $code;
+        self::$response["meta"]["message"] = $message;
+        self::$response["data"] = $data;
+
+        return response()->json(self::$response, self::$response["meta"]["code"]);
+    }
+}
